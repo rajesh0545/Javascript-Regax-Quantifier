72f8792c7fea9c8dc3223048e9ff1aec87a49c62
rajesh <krajeshgowri7032@mail.com>
2022-08-29 18:14:20 +0530

    Javscript-regax-quentififier

---
+++ script.js
@@ -0,0 +1,22 @@
+let text="hellooo world! Welocome To Over World ";
+let pattern = /o+/ig;
+let result = text.match (pattern);
+document.getElementById("quentifiers").innerHTML= result;
+
+
+
+
+// let text = "1,10, 100 or 1000?";
+// let pattern = /\d{4}/g;
+// let result = text.match(pattern);
+
+// document.getElementById("quen?").innerHTML = result;
+
+
+
+
+
+// let text = "Is this all there is";
+// let pattern = /is(?= all)/;
+// let result = text.match(pattern);
+// document.getElementById("quen?").innerHTML= result;
